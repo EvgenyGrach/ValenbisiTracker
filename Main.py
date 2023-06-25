ddd553483446128f1d01424964619c8e611f4626
egrache <evgeniygrachev@icloud.com>
2023-06-25 14:01:18 +0100

    Create Main.py

---
+++ Main.py
@@ -0,0 +1,13 @@
+from Try import mapita
+import streamlit as st
+from pages.Buscador import show_secondary_page
+
+
+
+def main():
+    # Navigation menu
+    st.title("Valenbisi Fast Tracker")
+   
+
+if __name__ == '__main__':
+    main()
