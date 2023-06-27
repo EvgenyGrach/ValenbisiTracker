db95fea5614c99f5c9ec056e506d511341866da4
egrache <evgeniygrachev@icloud.com>
2023-06-27 16:07:05 +0100

    Update Main.py

--- Main.py
+++ Main.py
@@ -4,15 +4,7 @@ import streamlit as st
 from pages.Buscador import show_secondary_page
 from pages.FGV_Tracker import show_third_page
 
-
-def main():
-    # Navigation menu
-    
-    st.set_page_config(
+st.set_page_config(
     page_title="FGV/Valenbisi Tracker",
     page_icon="👋"
 )
-   
-
-if __name__ == '__main__':
-    main()
