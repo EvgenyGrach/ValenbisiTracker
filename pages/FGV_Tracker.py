e8d908c13a79f14d995e09e9d801a5251b65f0d7
egrache <evgeniygrachev@icloud.com>
2023-06-27 15:22:54 +0100

    Update FGV_Tracker.py

--- pages/FGV_Tracker.py
+++ pages/FGV_Tracker.py
@@ -141,7 +141,7 @@ def show_third_page():
                         geom.append([n,i])
                         linea = folium.PolyLine([geom], color = 'red', weight = 3)
                         linea.add_to(map6)
-        map6.fit_bounds([(oeste)-0.02, (este)-0.03])        
+        map6.fit_bounds([oeste, este])        
         folium_static(map6)
 
 
