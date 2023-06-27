a6be09bacb31be1db7a17ce1dad2f6eef1d2c29d
egrache <evgeniygrachev@icloud.com>
2023-06-27 17:39:12 +0100

    Update Main.py

--- Main.py
+++ Main.py
@@ -15,3 +15,16 @@ f = f"""
 
 st.write(f, unsafe_allow_html=True)
 st.write("__________________________________________________________________________________")
+c1, c2 = st.columns(2)
+f = f"""
+    <span style="fomt-weight:bold;">Check info on FGV Stations at</span><br>
+    <span> FGV Tracker</span>
+    """
+c = f"""
+    <span style="fomt-weight:bold;">Check info on Valenbisi  at</span><br>
+    <span> Valenbisi Tracker</span>
+    """
+f = st.write(f)
+c1.text(f)
+c = st.write(c)
+c1.text(c)
