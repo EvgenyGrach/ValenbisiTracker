c906f9d2b6a82bc502d92ac136b1e7169698917b
egrache <evgeniygrachev@icloud.com>
2023-06-27 19:19:33 +0100

    Update Valenbisi_Tracker.py

--- pages/Valenbisi_Tracker.py
+++ pages/Valenbisi_Tracker.py
@@ -142,7 +142,9 @@ def mapita():
             st.dataframe(localizacion1)
             icon_color = get_icon_color(localizacion['available'].item())
             selected_lat = list(selected_lat)
+            st.write(selected_lat)
             selected_lat = selected_lat[0]
+            o
             selected_long = list(selected_long)
             selected_long = selected_long[0]
             route_geometry = get_route_geometry(latc, longc, selected_lat, selected_long)
